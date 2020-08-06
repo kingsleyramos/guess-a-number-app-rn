ae40aa6d763ccdda701baaf00d8a616dd31771a2
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-05 23:59:06 -0700

    added gamescreen logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card'
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -10,15 +12,58 @@ const generateRandomBetween = (min, max, exclude) => {
     } else return rndNum;
 }
 
-const GameScreen = (prop) => {
+const GameScreen = (props) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
+    
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = (direction) => {
+        if ((direction == 'lower' && currentGuess < props.userChoice) || 
+            (direction == 'greater' && currentGuess > props.userChoice)) {
+            Alert.alert('Dont\'t lie', 'You know this is wrong...', [
+                { 
+                    text: 'sorry!', 
+                    style: 'cancel'
+                }
+            ]);
+            return;
+        }
+
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+        setCurrentGuess(nextNumber)
+    }
+    
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.screen}>
+            <Text>Opponents Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title='LOWER' onPress={nextGuessHandler.bind(this, 'lower')}/>
+                <Button title='HIGHER' onPress={nextGuessHandler.bind(this, 'greater')}/>
+            </Card>
         </View>
     )
 }
 
 export default GameScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+})
