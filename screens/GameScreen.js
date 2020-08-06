cc2e317b17b5b42a415e1486c0de31fcdd1694b5
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-06 00:32:19 -0700

    added useEffect to count rounds

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { StyleSheet, Text, View, Button, Alert } from 'react-native'
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card'
@@ -14,9 +14,18 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = (props) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
-    
+    const [rounds, setRounds] = useState(0)
+
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props // Destruct the props in order to not use 'props' in useEffect
+
+    useEffect(() => {
+        if (currentGuess == userChoice) {
+            onGameOver(rounds)
+        }
+    }, [currentGuess, userChoice, onGameOver])
 
     const nextGuessHandler = (direction) => {
         if ((direction == 'lower' && currentGuess < props.userChoice) || 
@@ -37,6 +46,7 @@ const GameScreen = (props) => {
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setCurrentGuess(nextNumber)
+        setRounds(currentRounds => currentRounds + 1)
     }
     
     return (
