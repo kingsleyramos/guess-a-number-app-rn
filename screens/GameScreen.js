364c7bb500d312cff13c1e2a4896e863ea50f27a
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-07 09:16:01 -0700

    added and updated title from gobal defaultStyles

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native'
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card'
 
+import DefaultStyles from '../constants/default-styles'
+
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -51,7 +53,7 @@ const GameScreen = (props) => {
     
     return (
         <View style={styles.screen}>
-            <Text>Opponents Guess</Text>
+            <Text style={DefaultStyles.title}>Opponents Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
                 <Button title='LOWER' onPress={nextGuessHandler.bind(this, 'lower')}/>
