e97c9646d06afb5d08d37513cbf37f272fddd129
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-07 09:16:34 -0700

    updated updated with bodyText and TitleText

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,6 +5,8 @@ import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
 import NumberContainer from '../components/NumberContainer'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const StartGameScreen = (props) => {
     const [enteredValue, setEnteredValue] = useState('')
@@ -50,9 +52,9 @@ const StartGameScreen = (props) => {
             Keyboard.dismiss() // This will dismiss the keyboard if tapped elsewhere on the screen
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start New Game!</Text>
+                <TitleText style={styles.title}>Start New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text> Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input} 
                         blurOnSubmit 
                         autoCapitalize='none' 
@@ -87,7 +89,8 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
-        marginVertical: 10
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
