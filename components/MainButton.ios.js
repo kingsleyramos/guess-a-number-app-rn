1374230f765254be03e77d336d53bc00d8870f59
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 13:57:09 -0700

    Added ios.js

---
+++ components/MainButton.ios.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
+} from "react-native";
+
+import Colors from "../constants/colors";
+
+const MainButton = (props) => {
+
+    return (
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>{props.children}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default MainButton;
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden'
+    },
+    button: {
+        backgroundColor: Colors.primary,
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 25,
+    },
+    buttonText: {
+        color: "white",
+        fontFamily: "open-sans",
+        fontSize: 18,
+    },
+});
