136566ac5a5b67eb7c29abe76d8840f78d0c0ce0
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 13:56:11 -0700

    Added safeAreaView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 import Header from './components/Header'
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
@@ -54,10 +54,10 @@ export default function App(props) {
     
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Header title='Guess a Number' />
             {content}
-        </View>
+        </SafeAreaView>
     );
 }
 
