da75db8f80233475f3b2ef4ef9658d8815fc49d5
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 11:27:19 -0700

    updated styling for iOS and Android

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {StyleSheet, Text, View, Platform} from "react-native";
 
-import Colors from '../constants/colors'
-import defaultStyles from '../constants/default-styles'
+import Colors from "../constants/colors";
+import defaultStyles from "../constants/default-styles";
 
 const Header = (props) => {
-    return(
-        <View style={styles.header}>
-            <Text style={defaultStyles.title}>{props.title}</Text>
+    return (
+        <View
+            style={{
+                ...styles.header,
+                ...Platform.select({
+                    ios: styles.headerIOS,
+                    android: styles.headerAndroid,
+                }),
+            }}
+        >
+            <Text
+                style={{
+                    ...defaultStyles.title,
+                    ...styles.title,
+                }}
+            >
+                {props.title}
+            </Text>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
-    header:{
-        width: '100%',
+    header: {
+        width: "100%",
         height: 90,
         paddingTop: 36,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    headerIOS: {
+        backgroundColor: "white",
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 2,
+    },
+    headerAndroid: {
         backgroundColor: Colors.primary,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+    },
+    title: {
+        color: Platform.OS === "ios" ? Colors.primary : "white",
+    },
 });
 
 export default Header;
