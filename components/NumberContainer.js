dca7d5518618b6be34ca825510371f788e96aa1e
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 12:30:03 -0700

    disabled eslint react/prop-types

--- components/NumberContainer.js
+++ components/NumberContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 import Colors from "../constants/colors";
@@ -5,7 +6,7 @@ import Colors from "../constants/colors";
 const NumberContainer = (props) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.number}> {props.children} </Text>{" "}
+            <Text style={styles.number}> {props.children} </Text>
         </View>
     );
 };
