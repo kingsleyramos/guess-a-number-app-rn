9e4204a991b331826fd11f3998e7c458d02d29c7
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-07 17:18:04 -0700

    added dynamic dimensions

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, Text, View, Button, Alert, ScrollView, FlatList} from 'react-native'
+import { StyleSheet, Text, View, Button, Alert, ScrollView, FlatList, Dimensions} from 'react-native'
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card'
 import { Ionicons } from '@expo/vector-icons'
@@ -78,6 +78,12 @@ const GameScreen = (props) => {
         setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses])
     }
 
+    // Example for conditional Styles for Dimensions API
+    // let listContainerStyle = styles.listContainer
+    // if (Dimensions.get('window').width < 350){
+    //     listContainerStyle = styles.listContainerBig;
+    // }
+
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>Opponents Guess</Text>
@@ -117,13 +123,13 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 300,
         maxWidth: '90%'
     },
     listContainer: {
-        width: '60%',
-        flex: 1
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
+        flex: 1,
     },
     list:{
         flexGrow: 1, // more flexable that flex for lists
